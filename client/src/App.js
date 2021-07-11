7b13da657bc89e1335cbd846a3eeb2465ce0b7af
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-07-11 22:04:51 +0530

    login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-// import DeanForm from "./components/Admin/Form";
 import ClubForm from "./components/Club/Form";
 import FinanceForm from "./components/Finance/Form";
 import FacultyForm from "./components/Faculty/Form";
 import DeanForm from "./components/Dean/Form";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 const App = () => {
-  return <FinanceForm></FinanceForm>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          component={() => <Redirect to="/club/login"></Redirect>}
+        ></Route>
+        <Route path="/club/login" component={ClubForm} />
+        <Route path="/finance/login" component={FinanceForm} />
+        <Route path="/faculty/login" component={FacultyForm} />
+        <Route path="/dean/login" component={DeanForm} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
